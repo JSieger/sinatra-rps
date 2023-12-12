d1168908a4e3245aa11f699a6978cbac06888f6f
Joseph Sieger <jsieger44@gmail.com>
2023-12-12 20:09:59 +0000

    basic logic for the game

--- app.rb
+++ app.rb
@@ -7,3 +7,20 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  # 1 = rock, 2 = paper, 3 = scissors
+  cpu_move = rand(1..3)
+  outcome = ""
+
+  if cpu_move == 1
+    outcome = "tied"
+  elsif cpu_move == 2
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
+
+  pp cpu_move
+  pp outcome
+end
