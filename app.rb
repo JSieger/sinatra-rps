30d6882c5ff71a93e505cbcbe3956daf65d282e3
Joseph Sieger <jsieger44@gmail.com>
2023-12-13 20:01:52 +0000

    scissors done, we cut the bug in half

--- app.rb
+++ app.rb
@@ -79,3 +79,39 @@ get("/paper") do
   </a>
   "
 end
+
+get("/scissors") do
+  # 1 = rock, 2 = paper, 3 = scissors
+  cpu_move = rand(1..3)
+  move_text = ""
+  outcome = ""
+
+  if cpu_move == 1
+    move_text = "rock"
+    outcome = "lost"
+  elsif cpu_move == 2
+    move_text = "paper"
+    outcome = "won"
+  else
+    move_text = "scissors"
+    outcome = "tied"
+  end
+
+  "
+  <h2>
+    We played scissors!
+  </h2>
+
+  <h2>
+   They played #{move_text}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>
+
+  <a href='/'>
+    Rules
+  </a>
+  "
+end
