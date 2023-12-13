0b472671f177556675b20863605982fc5890ca45
Joseph Sieger <jsieger44@gmail.com>
2023-12-13 20:18:08 +0000

    paper working with layout, PAPER BOII

--- app.rb
+++ app.rb
@@ -31,37 +31,21 @@ end
 get("/paper") do
   # 1 = rock, 2 = paper, 3 = scissors
   cpu_move = rand(1..3)
-  move_text = ""
-  outcome = ""
+  @move_text = ""
+  @outcome = ""
 
   if cpu_move == 1
-    move_text = "rock"
-    outcome = "won"
+    @move_text = "rock"
+    @outcome = "won"
   elsif cpu_move == 2
-    move_text = "paper"
-    outcome = "tied"
+    @move_text = "paper"
+    @outcome = "tied"
   else
-    move_text = "scissors"
-    outcome = "lost"
+    @move_text = "scissors"
+    @outcome = "lost"
   end
 
-  "
-  <h2>
-    We played paper!
-  </h2>
-
-  <h2>
-   They played #{move_text}!
-  </h2>
-
-  <h2>
-    We #{outcome}!
-  </h2>
-
-  <a href='/'>
-    Rules
-  </a>
-  "
+  erb(:paper)
 end
 
 get("/scissors") do
